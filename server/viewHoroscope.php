9700c3d3595d28bc41b1ba3a235e82fe0b52cf20
Grenhub <Ossi_gren@hotmail.com>
2020-12-18 18:17:20 +0100

    #1

---
+++ server/viewHoroscope.php
@@ -0,0 +1,34 @@
+<?php
+
+try {
+
+    require("./addHoroscope.php");
+    session_start();
+    
+    if (isset($_SERVER["REQUEST_METHOD"])) {
+
+        if ($_SERVER["REQUEST_METHOD"] === "GET") {
+
+            if (isset($_SESSION["horoscope"]))  {
+
+                echo json_encode(true);
+                exit;
+                 
+            } if (!isset($_SESSION["horoscope"])) {
+                
+                echo json_encode(false);  
+                exit;              
+            } 
+
+        } 
+    }
+    
+} catch(Exception $error) {
+    echo json_encode(
+        array(
+            "Message" => $error -> getMessage(),
+            "Success" => false 
+            )
+    );
+    exit;
+}
